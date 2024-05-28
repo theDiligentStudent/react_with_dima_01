51be869d764649973afb34b193bf6dd61f7cc381
theDiligentStudent <ivanrenkin14@gmail.com>
2024-05-28 13:38:11 +0300

    Lesson 6 part 3 ---- 28/05/24 ---- посмотрел ВЕСЬ  Ур.-а Димыча --- поработал над ним сам хорошенько, поповторял, максимально разобрал.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
-
 const App = () => {
   return (
-    // <div className='App'>
-    <div>
-      <p> Hello, Friends!</p>
-      <p> Simple HTML</p>
+    // <div></div>   ----- НЕ допустимо!!!!!!!
+    // Должен быть ---- ТОЛЬКО ОДИН ---- Корневой Узел
+    // return должен Возвращать  ---- ЛИШЬ ОДНО - З. ---
+    //   в нашем случае это - div-ка
+    //      а ВНУТРИ НЕЁ --- уже  --- ЛЕЖИТ- В С Ё  ОСТАЛЬНОЕ
+
+    <div className='App'>
+      {/* <div> */}
+      <ul>
+        <li>html</li>
+        <li>css</li>
+        <li>js</li>
+        <li>React</li>
+      </ul>
     </div>
   );
 };
