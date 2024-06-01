986f7d5b49651e07c451bfd09a57d1836b654693
theDiligentStudent <ivanrenkin14@gmail.com>
2024-06-01 21:41:37 +0300

    Lesson 10 - default - опасность ---- 1/06/24 --- Изменнений в Фа.-х практически Нет. Сделал коммит после очередного Ур.-а на всякий случай.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
-// import Header from './components/Header';  // Правильный вариант БЕЗ .js
 import Header from './components/Header.js';
+// import Header from './components/Header';  // Правильный вариант БЕЗ .js
+
+
+// НЕПРИЯТНАЯ ОШИБКА!!!!!
+//   НЕПОКАЗЫВАЕМАЯ нам редактором......
+//   ----
+//   Ком.-т (по Ош.-е - НЕдосмотру ВЗЯТА НЕ ИЗ ТОГО Фа.-а !!!!!)
+// import Header from './components/Technologies.js';
+
+import Technologies from './components/Technologies.js'
 // import Technologies from './components/Technologies'; // Правильный вариант БЕЗ .js
-import Technologies from './components/Technologies.js';
 
 
 const App = () => {
