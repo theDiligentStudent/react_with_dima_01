8c6abce9499d2eb7e3f63a8e8f13f10833fa42fa
theDiligentStudent <ivanrenkin14@gmail.com>
2024-06-01 16:17:11 +0300

    Lesson 9  part 0 - Import - Export наших Ком.-ет  ---- 1/06/24 --- Сперва сделал САМ, то Что Дима собирается делать в Ур.-е. ---- Создал Отдельную Пп.-у для Ком.-т - создал в ней два отдельных Фа.-а Header.js - for Component Header and file Technologies.js for Component Technologies. Экспортировал Ком.-ты из Их Фа.-в ---- Экспортировал их в Фа.-е App.js ----- Всё Прекрасно РаботаеТ!!!!

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './components/Header';
+import Technologies from './components/Technologies';
+
+
 const App = () => {
   return (
     <div>
@@ -13,27 +17,27 @@ const App = () => {
 
 const Head = () => {};
 
-const Header = () => {
-  return (
-    <div>
-      <a href='#s'>Home </a>
-      <a href='#'> Messages </a>
-      <a href='#'> About Us</a>
-    </div>
-  );
-};
+// const Header = () => {
+//   return (
+//     <div>
+//       <a href='#s'>Home </a>
+//       <a href='#'> Messages </a>
+//       <a href='#'> About Us</a>
+//     </div>
+//   );
+// };
 
-const Technologies = () => {
-  return (
-    <div>
-      <ul>
-        <li>html</li>
-        <li>css</li>
-        <li>js</li>
-        <li>react</li>
-      </ul>
-    </div>
-  );
-};
+// const Technologies = () => {
+//   return (
+//     <div>
+//       <ul>
+//         <li>html</li>
+//         <li>css</li>
+//         <li>js</li>
+//         <li>react</li>
+//       </ul>
+//     </div>
+//   );
+// };
 
 export default App;
---
+++ my-app/src/components/Header.js
@@ -0,0 +1,12 @@
+
+const Header = () => {
+    return (
+      <div>
+        <a href='#s'>Home </a>
+        <a href='#'> Messages </a>
+        <a href='#'> About Us</a>
+      </div>
+    );
+  };
+
+  export default Header
