d086b73c9a97f35c43c57bc421828ccdf18ee7a7
theDiligentStudent <ivanrenkin14@gmail.com>
2024-06-10 18:06:40 +0300

    Lesson 15  part 3_1   ---- 10/06/24  ----  пришлось немного (но ОЧЕНЬ-ОЧЕНЬ ДОЛГО) ПЕРЕДЕЛЫВАТЬ  CSS. Правда НЕ самостоятельно --- а с помощью ДжиПиТи ----- НЕКОРЕКТНО работала аватарка универсального поста. ...... Я даже и не заметил этого - перед тем - как делать Предыдущий комм. пока не понадобилась Перезагрузка страницы - и не вскрылась Ош. (в устаревших технологиях.....  ----- Сейчас (кажется) всё уже работает нормальн.....

--- my-app/src/components/Profile/my_posts/post/Post.jsx
+++ my-app/src/components/Profile/my_posts/post/Post.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import s from './Post.module.css';
-import img_4 from './../../../../images/project_1/image_4.jpg';
+import img_4 from '../../../../images/project_1/image_4.jpg';
 
 const Post = () => {
   return (
     <div className={s.item}>
-      <img src={img_4} />
+      <img className={s.image} src={img_4} />
+      {/* <img src={img_4} /> */}
       Post 1
     </div>
   );
