aaba54a193ebf50d31be4d4d6be65208f67dc415
theDiligentStudent <ivanrenkin14@gmail.com>
2024-06-16 13:46:55 +0300

    Lesson 18  part 0   ---- 16/06/24 ---  Вчера посмотрел КОНСПЕКТ этого Ур.-а --- сегодня повторил --- кажется Всё -- По Памяти.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,13 +4,18 @@ import './App.css';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import Profile from './components/Profile/Profile';
+import Dialogues from './components/Dialogues/Dialogues';
 
 const App = () => {
   return (
     <div className='app-wrapper'>
       <Header />
       <Sidebar />
-      <Profile />
+      {/* пока Комментируем эту Ком.-у --- (думаю - до Следующего В.) -- чтобы можно было работать/видеть с Новой Ком.-й Dialogues */}
+      {/* <Profile /> */}
+
+      {/* создаю НОВУЮ Ком.-у для Сообщений */}
+      <Dialogues />
     </div>
   );
 };
---
+++ my-app/src/components/Dialogues/Dialogues.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import s from './Dialogues.module.css';
+
+const Dialogues = () => {
+  return (
+    <div className='wrapper-content'>
+      <div className={s.messages_color}>Messages</div>
+    </div>
+  );
+};
+
+export default Dialogues;
