001af3f7f45e78f8c48853915564d6c6ecde697d
theDiligentStudent <ivanrenkin14@gmail.com>
2024-06-25 12:38:16 +0300

    Lesson 20  ---- 25/06/24 --- Посмотрел Ур. Димы -- часть попробовал поделать САМ. Мне кажется - материал - более-менее Усвоил.

--- my-app/src/components/Sidebar/Sidebar.jsx
+++ my-app/src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import s from './Sidebar.module.css';
 
@@ -9,20 +10,22 @@ console.log(s);
 const Sidebar = () => {
   return (
     <nav className={s.nav}>
-      <div className={`${s.item} ${s.active}`}>
-        <a href='/Profile'>Profile</a>
+      {/* <div className={`${s.item} ${s.active}`}> */}
+      <div className={s.item}>
+        <NavLink to='/Profile' className={navData=>navData.isActive ? s.activeLink: s.item}>Profile</NavLink>
+        {/* <NavLink to='/Profile'>Profile</NavLink> */}
       </div>
       <div className={s.item}>
-        <a href='/Dialogues'>Messages</a>
+        <NavLink to='/Dialogues' className={navData=>navData.isActive ?s.activeLink: s.item}>Messages</NavLink>
       </div>
       <div className={s.item}>
-        <a href='/News'>News</a>
+        <NavLink to='/News'>News</NavLink>
       </div>
       <div className={s.item}>
-        <a href='/Guests'>Guests</a>
+        <NavLink to='/Guests'>Guests</NavLink>
       </div>
       <div className={s.item}>
-        <a href='/Settings'>Settings</a>
+        <NavLink to='/Settings' className={navData=>navData.isActive ? s.activeLink: s.item}>Settings</NavLink>
       </div>
     </nav>
   );
